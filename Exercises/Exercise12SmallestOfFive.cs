83616046172eb7e3183705e57a1b9d82595a681d
Juan Pablo Martinez Fernandez <juan.martinezf@grupofi.com.co>
2026-09-10 10:48:59 -0500

    feat: implementar ejercicios 11 y 12

---
+++ Exercises/Exercise12SmallestOfFive.cs
@@ -0,0 +1,30 @@
+﻿namespace ActividadProgramacionCSharp.Exercises
+{
+    public class Exercise12SmallestOfFive
+    {
+        public void Ejecutar()
+        {
+            Console.WriteLine("=== EJERCICIO 12: SMALLEST OF FIVE ===");
+
+            double[] numeros = new double[5];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Ingrese el número {i + 1}: ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double menor = numeros[0];
+
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+            }
+
+            Console.WriteLine($"Resultado: {menor}");
+        }
+    }
+}
